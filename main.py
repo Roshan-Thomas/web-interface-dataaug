75d31f85dc9db663921dc85c317005b89dcb665d
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-06-11 22:20:32 +0300

    Major Updates: - Back translating implemented - Farasa API usage - Better commenting for model.py - Pipfile updated - UI changes and better description in main page - Caching for Loaded models for faster running

--- main.py
+++ main.py
@@ -1,39 +1,128 @@
 import streamlit as st 
-from model import text_generation, fill_mask
+from model import aug_bert, aug_GPT, aug_w2v, back_translate
 
-# for now only doing arabert and aragpt2 models 
-# TODO: aravec model
+st.set_page_config(
+     page_title="Data Augmentation",
+     page_icon='📈'
+ )
 
-st.title("Web Interface with araGPT2")
+st.title("Data augmentation - Web Interface")
+st.markdown(
+    """
+    Welcome to our data augmentation web interface.
 
-# TODO: w2v function
-# TODO: aravec models
+    ### What is Data Augmentation?
 
-user_text_input = st.text_input("Enter Text here (AR):")
+    It is a set of techniques used in data analysis to increase the amount of 
+    data by adding slightly modified copies of already existing data or newly
+    created synthetic data from existing data. Read more [here.](https://en.wikipedia.org/wiki/Data_augmentation)
 
-if user_text_input:
-    # Tell the user that the data is loading
-    loading_state = st.text("Loading data...")
+    We are using four methods to do data augmentation on Arabic text: 
+        
+        * AraBERT (Machine Learning Model)
+        * AraGPT2 (Machine Learning Model)
+        * W2V (Machine Learning Model)
+        * Back Translation
+    """
+)
 
-    # GPT2-based text generation models
-    aragpt2 = text_generation('aubmindlab/aragpt2-medium',user_text_input) ## use the mega model
+test_app_container = st.container()
 
-    # BERT-based fill mask models (4)
-    # arabert = fill_mask('aubmindlab/bert-base-arabert', user_text_input)
-    # arabertv2 = fill_mask('aubmindlab/bert-large-arabertv2',user_text_input)
-    # arabertv02 = fill_mask('aubmindlab/bert-large-arabertv02',user_text_input)
-    # arabertv01 = fill_mask('aubmindlab/bert-base-arabertv01',user_text_input)
+with test_app_container:
+  st.markdown("# Test out our app here :blush::")
+  user_text_input = st.text_input("Enter your text here (AR):")
+  # test_text = "RT @USER: رحمك الله يا صدام يا بطل ومقدام. URL	NOT_OFF	NOT_HS" # text to be used for testing purposes only
 
-    # Inform the user the data is loaded
-    loading_state.text("Data Loaded ✅")
+  if user_text_input:
+    bert_container = st.container()
+    gpt2_container = st.container()
+    w2v_container = st.container()
+    back_translation_container = st.container()
 
-    output = "org: " + text + "\n"
-    st.write(output)
+    with bert_container:
+      st.subheader("AraBERT Data Augmentation")
+      sentences_bert = aug_bert('aubmindlab/bert-large-arabertv2', user_text_input)
+      with st.expander("Open to see AraBERT results"):
+        st.write(sentences_bert)
 
-    gpt = list(set(aragpt2))
-    for aug in gpt:
-        st.write(f"GPT2-based: {aug} \n")
+    with gpt2_container:
+      st.subheader("AraGPT2 Data Augmentation")
+      sentences_gpt = aug_GPT('aubmindlab/aragpt2-medium', user_text_input)
+      with st.expander("Open to see AraGPT2 results"):
+        st.write(sentences_gpt)
 
-    # bert = list(set(arabert) | set(arabertv2) | set(arabertv02) | set(arabertv01))
-    # for aug in bert:
-    #     st.write(f"BERT-based: {aug}\n ")
+    with w2v_container:
+      st.subheader("W2V Data Augmentation")
+      sentences_w2v = aug_w2v('./data/full_grams_cbow_100_twitter.mdl', user_text_input)
+      with st.expander("Open to see W2V results"):
+        st.write(sentences_w2v)
+
+    with back_translation_container:
+      st.markdown("### Back Translation Augmentation")
+      available_languages = ['ar-en', 'ar-fr', 'ar-tr', 'ar-ru', 'ar-pl', 'ar-it', 'ar-es', 'ar-el', 'ar-de', 'ar-he']
+      back_translated_sentences = []
+      st.markdown(
+          """
+          The languages we are back translating from are: *English (en), French (fr), 
+          Turkish (tr), Russian (ru), Polish (pl), Italian (it), Spanish (es), 
+          Greek (el), German (de), and Hebrew (he)*.
+          """
+      )
+
+      back_translated_sentences = back_translate(available_languages, user_text_input)
+      with st.expander("Open to see Back Translation results"):
+          st.write(back_translated_sentences)
+
+st.write("-------------------------------------------------")
+
+st.header("Citations")
+with st.expander("Expand to see the citations"):
+  aragpt2_citation = '''
+    @inproceedings{antoun-etal-2021-aragpt2,
+      title = "{A}ra{GPT}2: Pre-Trained Transformer for {A}rabic Language Generation",
+      author = "Antoun, Wissam  and
+        Baly, Fady  and
+        Hajj, Hazem",
+      booktitle = "Proceedings of the Sixth Arabic Natural Language Processing Workshop",
+      month = apr,
+      year = "2021",
+      address = "Kyiv, Ukraine (Virtual)",
+      publisher = "Association for Computational Linguistics",
+      url = "https://www.aclweb.org/anthology/2021.wanlp-1.21",
+      pages = "196--207",
+    }
+  '''
+  arabert_citation = '''
+    @inproceedings{antoun2020arabert,
+      title={AraBERT: Transformer-based Model for Arabic Language Understanding},
+      author={Antoun, Wissam and Baly, Fady and Hajj, Hazem},
+      booktitle={LREC 2020 Workshop Language Resources and Evaluation Conference 11--16 May 2020},
+      pages={9}
+    }
+  '''
+  w2v_citation = '''
+    @article{article,
+      author = {Mohammad, Abu Bakr and Eissa, Kareem and El-Beltagy, Samhaa},
+      year = {2017},
+      month = {11},
+      pages = {256-265},
+      title = {AraVec: A set of Arabic Word Embedding Models for use in Arabic NLP},
+      volume = {117},
+      journal = {Procedia Computer Science},
+      doi = {10.1016/j.procs.2017.10.117}
+    }
+  '''
+
+  bt_citation = '''
+    @InProceedings{TiedemannThottingal:EAMT2020,
+      author = {J{\"o}rg Tiedemann and Santhosh Thottingal},
+      title = {{OPUS-MT} — {B}uilding open translation services for the {W}orld},
+      booktitle = {Proceedings of the 22nd Annual Conferenec of the European Association for Machine Translation (EAMT)},
+      year = {2020},
+      address = {Lisbon, Portugal}
+    }
+  '''
+  st.code(aragpt2_citation)
+  st.code(arabert_citation)
+  st.code(w2v_citation)
+  st.code(bt_citation)
--- model.py
+++ model.py
@@ -3,59 +3,282 @@ import json
 import requests
 from transformers import GPT2LMHeadModel, pipeline, GPT2TokenizerFast
 import gensim
+import re
+import time
+import nlpaug.augmenter.word as naw
 
-@st.cache(suppress_st_warning=True)
-def text_generation(model_name, text):
-    punc = "،.:!?"
-    text = text.strip()
-    if text[-1] in punc:
-        text = text[0:-1]
-    text = text.strip()
-    model = GPT2LMHeadModel.from_pretrained(model_name)
-    tokenizer = GPT2TokenizerFast.from_pretrained(model_name)
-    generation_pipeline = pipeline("text-generation",model=model,tokenizer=tokenizer)
-    l = []
-    #method 1
+### ------------------------ Back Translation ------------------------------ ###
+
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+st.cache(allow_output_mutation=True)
+def load_models_bt(from_model_name, to_model_name):
+  back_translation = naw.BackTranslationAug(
+      from_model_name=from_model_name,
+      to_model_name=to_model_name,
+      device='cuda'
+  )
+  return (back_translation)
+
+def back_translate(selected_languages, text):
+  """
+  List of Languages Used
+    - ar-en	(English)
+    - ar-fr	(French)	
+    - ar-tr	(Turkish)
+    - ar-ru	(Russian)
+    - ar-pl	(Polish)
+    - ar-it	(Italian)
+    - ar-es	(Spanish)
+    - ar-el (Greek)
+    - ar-de (German)
+    - ar-he (Hebrew)
+  """
+
+  all_sentences = []
+  available_languages = ['ar-en', 'ar-fr', 'ar-tr', 'ar-ru', 'ar-pl', 'ar-it', 'ar-es', 'ar-el', 'ar-de', 'ar-he']
+
+  loading_state_bt = st.text("Loading & Augmenting Back Translating Models...")
+  tic = time.perf_counter()
+
+  for model in available_languages:
+    model_name = model.split('-')
+    back_translation = load_models_bt(f'Helsinki-NLP/opus-mt-{model_name[0]}-{model_name[1]}',
+                                      f'Helsinki-NLP/opus-mt-{model_name[1]}-{model_name[0]}')
+    bt_sentence = back_translation.augment(text)
+    all_sentences.append(bt_sentence)
+
+  toc = time.perf_counter()
+  loading_state_bt.write("Loading & Augmenting Back Translating Models done ✅: " + str(toc-tic) + " seconds")
+
+  return all_sentences
+
+### ---------------------- End of Back Translation ------------------------- ###
+
+### -------------------------------- GPT ------------------------------------ ###
+
+@st.cache(allow_output_mutation=True)
+def load_GPT(model_name):
+  model = GPT2LMHeadModel.from_pretrained(model_name)
+  tokenizer = GPT2TokenizerFast.from_pretrained(model_name)
+  generation_pipeline = pipeline("text-generation",model=model,tokenizer=tokenizer)
+  return model , tokenizer , generation_pipeline
+
+def GPT(model,tokenizer , generation_pipeline ,sentence):
+  org_text = sentence
+  sentence = process(sentence)
+  l = []
+  if len(sentence.split()) < 11:
+    input_ids = tokenizer.encode(sentence, return_tensors="pt")
     for n in range(1,4):
-        for i in range(5):
-            pred = generation_pipeline(text,
-                return_full_text = True,
-                pad_token_id=tokenizer.eos_token_id,
-                num_beams=10 ,
-                max_length=len(text.split()) + n,
-                top_p=0.9,
-                repetition_penalty = 3.0,
-                no_repeat_ngram_size = 3)[0]['generated_text'].replace("."," ").replace("،"," ").replace(":"," ").strip()
-            pred = " ".join(pred.split())
-            if not pred in l:
-                l.append(pred)
-    # method 2
-    text = " ".join(text.split()[0:-1])
-    for n in range(1,4):
-        for i in range(5):
-            pred = generation_pipeline(text,
-                return_full_text = True,
-                pad_token_id=tokenizer.eos_token_id,
-                num_beams=10 ,
-                max_length=len(text.split()) + n,
-                top_p=0.9,
-                repetition_penalty = 3.0,
-                no_repeat_ngram_size = 3)[0]['generated_text'].replace("."," ").replace("،"," ").replace(":"," ").strip()
-            pred = " ".join(pred.split())
-            if not pred in l:
-                l.append(pred)
+      for i in range(2):
+        pred = generation_pipeline(sentence,
+          return_full_text = True,
+          pad_token_id=tokenizer.eos_token_id,
+          num_beams=10 ,
+          max_length=len(input_ids[0]) + n,
+          top_p=0.9,
+          repetition_penalty = 3.0,
+          no_repeat_ngram_size = 3)[0]['generated_text'].replace("."," ").replace("،"," ").replace(":"," ").strip()
+        pred = " ".join(pred.split())
+        pred = org_text.replace(sentence,pred)
+        if not pred in l and not pred == org_text:
+          l.append(pred)
+  return l
 
-    return l
+def aug_GPT(model_name,text):  # text here can be list of sentences or on string sentence
+  loading_state_gpt = st.text("Loading AraGPT2...")
+  tic = time.perf_counter()
+  model , tokenizer , generation_pipeline = load_GPT(model_name)
+  toc = time.perf_counter()
+  loading_state_gpt.write("Loading AraGPT2 done ✅: " + str(toc-tic) + " seconds")
+  augment_state_gpt = st.text("Augmenting with AraGPT2...")
+  tic = time.perf_counter()
+  if isinstance(text, str):
+    ret = GPT(model,tokenizer , generation_pipeline ,text)
+    toc = time.perf_counter()
+    augment_state_gpt.text("Augmenting with AraGPT2 done ✅: " + str(toc-tic) + " seconds")
+    return ret
+  else:
+    all_sentences = []
+    for sentence in text:
+      sentence = sentence.strip()
+      all_sentences.append([sentence,GPT(model,tokenizer , generation_pipeline ,sentence)])
+    toc = time.perf_counter()
+    augment_state_gpt.text("Augmenting with AraGPT2 done ✅: " + str(toc-tic) + " seconds")
+    return all_sentences
 
-@st.cache(suppress_st_warning=True)
-def fill_mask(model, text):
-    model = pipeline('fill-mask', model= model)
-    l = []
-    for token in text.split()[1:]:
-        masked_text = text.replace(token,"[MASK]")
-        pred = model(masked_text , top_k = 20)
-        for i in pred:
-            output = i['token_str']
-            if not len(output) < 2 and not "+" in output and not "[" in output:
-                l.append(text.replace(token, i['token_str']))
-    return l
+### ------------------------ End of GPT ------------------------------------ ###
+
+### ------------------------------- W2V ------------------------------------ ###
+
+@st.cache(allow_output_mutation=True)
+def load_w2v(model_path):
+  try:
+      model = gensim.models.KeyedVectors.load_word2vec_format(model_path,binary=True,unicode_errors='ignore')
+  except:
+      model = gensim.models.Word2Vec.load(model_path)
+  return model
+
+def w2v(model,sentence):
+  org_text = sentence
+  sentence = process(sentence)
+  l = []
+  augs = []
+  if len(sentence.split()) < 11:
+    for token in sentence.split():
+      try:
+        word_vectors = model.wv
+        if token in word_vectors.key_to_index:
+           exist = True
+        else:
+           exist = False
+      except:
+        if token in model:
+          exist = True
+        else:
+          exist = False
+      if is_replacable(token,pos(sentence)):
+        if exist:
+          try:
+            most_similar = model.wv.most_similar( token, topn=5 )
+          except:
+            most_similar = model.most_similar( token, topn=5 )
+          for term, score in most_similar:
+                if term != token:
+                    term = term.replace("_"," ")
+                    aug = sentence.replace(token,term)
+                    if not aug.replace(".","").replace("،","").replace("!","").replace("؟","").replace(":","") in augs:
+                      augs.append(aug.replace(".","").replace("،","").replace("!","").replace("؟","").replace(":",""))
+                      l.append(org_text.replace(sentence,aug))
+  return l
+
+def aug_w2v(model_path,text):   # text here is a list of sentences
+  loading_state_w2v = st.text("Loading W2V...")
+  tic = time.perf_counter()
+  model = load_w2v(model_path)
+  toc = time.perf_counter()
+  loading_state_w2v.text("Loading W2V done ✅: " + str(toc-tic) + " seconds")
+  augment_state_w2v = st.text("Augmenting with W2V...")
+  tic = time.perf_counter()
+  if isinstance(text, str):
+    ret = w2v(model,text)
+    toc = time.perf_counter()
+    augment_state_w2v.text("Augmenting with W2V done ✅: " + str(toc-tic) + " seconds")
+    return ret
+  else:
+    all_sentences = []
+    for sentence in text:
+       sentence = sentence.strip()
+       all_sentences.append([sentence,w2v(model,sentence)])
+    toc = time.perf_counter()
+    augment_state_w2v.text("Augmenting with W2V done ✅: " + str(toc-tic) + " seconds")
+    return all_sentences
+
+
+### ------------------------ End of W2V ------------------------------------ ###
+
+### ------------------------------- BERT ----------------------------------- ###
+
+@st.cache(allow_output_mutation=True)
+def load_bert(model):
+  model = pipeline('fill-mask', model=model)
+  return model
+
+def bert(model, sentence):  # Contextual word embeddings
+  org_text = sentence
+  sentence = process(sentence)
+  l = []
+  augs = []
+  if len(sentence.split()) < 11:
+    for token in sentence.split():
+        if is_replacable(token,pos(sentence)):
+          masked_text = sentence.replace(token,"[MASK]")
+          pred = model(masked_text , top_k = 5)
+          for i in pred:
+            if isinstance(i, dict):
+              output = i['token_str']
+              if not output == token:
+                if not len(output) < 2 and not "+" in output and not "[" in output:
+                  aug = sentence.replace(token, i['token_str'])
+                  if not aug.replace(".","").replace("،","").replace("!","").replace("؟","").replace(":","") in augs:
+                        augs.append(aug.replace(".","").replace("،","").replace("!","").replace("؟","").replace(":",""))
+                        l.append(org_text.replace(sentence,aug))
+  return l
+
+def aug_bert(model,text):  # text here is a list of sentences
+  loading_state_bert = st.text("Loading AraBERT...")
+  tic = time.perf_counter()
+  model = load_bert(model)
+  toc = time.perf_counter()
+  loading_state_bert.text("Loading AraBERT done ✅: " + str(toc-tic) + " seconds")
+  augment_state_bert = st.text("Augmenting with AraBERT...")
+  tic = time.perf_counter()
+  if isinstance(text, str):
+    ret = bert(model, text)
+    toc = time.perf_counter()
+    augment_state_bert.text("Augmenting with AraBERT done ✅: " + str(toc-tic) + " seconds")
+    return ret
+  else:
+    all_sentences = []
+    for sentence in text:
+      sentence = sentence.strip()
+      all_sentences.append([sentence,bert(model, sentence)])
+    toc = time.perf_counter()
+    augment_state_bert.text("Augmenting with AraBERT done ✅: " + str(toc-tic) + " seconds")
+    return all_sentences
+
+### ------------------------ End of BERT ----------------------------------- ###
+
+### ------------------------ General Functions ----------------------------- ###
+
+def process(text):
+  # remove any punctuations in the text
+  punc = "،.:!?"
+  text = text.strip()
+  if text[-1] in punc:
+      text = text[0:-1]
+  text = text.strip()
+  # keep only arabic text
+  text = " ".join(re.findall(r'[\u0600-\u06FF]+', text))
+  return text
+
+
+def is_replacable(token,pos_dict):
+   if token in pos_dict:
+    if bool(set(pos_dict[token].split("+")) & set(['NOUN','V','ADJ'])):
+      return True
+   return False
+
+### ----------------- End of General Functions ----------------------------- ###
+
+### ------------------------- Farasa API ----------------------------------- ###
+
+def pos(text):
+  url = 'https://farasa.qcri.org/webapi/pos/'
+  api_key = "KMxvdPGsKHXQAbRXGL"
+  payload = {'text': text, 'api_key': api_key}
+  data = requests.post(url, data=payload)
+  result = json.loads(data.text)
+  text  = text.split()
+  pos_dict  = {}
+  for n in range(len(result["text"])):
+    i = result["text"][n]
+    if "+" == i['surface'][0]:
+      word = "".join(s.strip() for s in result["text"][n-1]['surface'].split("+"))
+      word = word + i['surface'].replace("+","").strip()
+      if word in text:
+        pos_dict[word] = result["text"][n-1]['POS']
+    if "+" == i['surface'][-1]:
+      word = "".join(s.strip() for s in result["text"][n+1]['surface'].split("+"))
+      word = i['surface'].replace("+","").strip() + word
+      if word in text:
+       pos_dict[word] = result["text"][n+1]['POS']
+    else:
+      word = "".join(s.strip() for s in i['surface'].split("+"))
+      if word in text:
+        pos_dict[word] = i['POS']
+  return pos_dict
+
+### ------------------------- End of Farasa API ---------------------------- ###
