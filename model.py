4f39374a76847ee8a712790be21f32da3d481c2f
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-06-21 12:47:19 +0300

    Fixed - Edge case where the augmented output is an empty list and the similarity table is not displayed

--- model.py
+++ model.py
@@ -513,40 +513,42 @@ def load_similarity_checker_model():
 
 def similarity_checker(sentences, user_text_input):
   tokenizer, model = load_similarity_checker_model()
-  tokens = {'input_ids': [], 'attention_mask': []}
-  sentences.insert(0, user_text_input)
-  for sentence in sentences:
-    # tokenize sentence and append to dictionary lists
-    new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,
-                                      padding='max_length', return_tensors='pt')
-    tokens['input_ids'].append(new_tokens['input_ids'][0])
-    tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+  if (len(sentences) > 0):
+    tokens = {'input_ids': [], 'attention_mask': []}
+    sentences.insert(0, user_text_input)
+    for sentence in sentences:
+      # tokenize sentence and append to dictionary lists
+      new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,
+                                        padding='max_length', return_tensors='pt')
+      tokens['input_ids'].append(new_tokens['input_ids'][0])
+      tokens['attention_mask'].append(new_tokens['attention_mask'][0])
 
-  # reformat list of tensors into single tensor
-  tokens['input_ids'] = torch.stack(tokens['input_ids'])
-  tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+    # reformat list of tensors into single tensor
+    tokens['input_ids'] = torch.stack(tokens['input_ids'])
+    tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
 
-  outputs = model(**tokens)
-  embeddings = outputs.last_hidden_state
-  attention_mask = tokens['attention_mask']
-  mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+    outputs = model(**tokens)
+    embeddings = outputs.last_hidden_state
+    attention_mask = tokens['attention_mask']
+    mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
 
-  masked_embeddings = embeddings * mask 
-  summed = torch.sum(masked_embeddings, 1)
-  summed_mask = torch.clamp(mask.sum(1), min=1e-9)
-  mean_pooled = summed / summed_mask
+    masked_embeddings = embeddings * mask 
+    summed = torch.sum(masked_embeddings, 1)
+    summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+    mean_pooled = summed / summed_mask
 
-  # Convert from PyTorch tensor to numpy array
-  mean_pooled = mean_pooled.detach().numpy()
+    # Convert from PyTorch tensor to numpy array
+    mean_pooled = mean_pooled.detach().numpy()
 
-  # Calculate cosine similarity
-  cos_similarity = cosine_similarity([mean_pooled[0]], mean_pooled[1:])
+    # Calculate cosine similarity
+    cos_similarity = cosine_similarity([mean_pooled[0]], mean_pooled[1:])
 
-  return cos_similarity[0]
+    return cos_similarity[0]
 
 def display_similarity_table(sentences_list, similarity_list):
-  data = list(zip(sentences_list, similarity_list))
-  df = pd.DataFrame(data, columns=['Sentences', 'Similarity Score'])
-  st.table(df) #TODO: Styling
+  if len(sentences_list) > 0:
+    data = list(zip(sentences_list, similarity_list))
+    df = pd.DataFrame(data, columns=['Sentences', 'Similarity Score'])
+    st.table(df) #TODO: Styling
 
 ### -------------------- End of Similarity Checker ------------------------- ###
