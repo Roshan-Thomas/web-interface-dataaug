730f75908a096aba540a8cb1fd86cf30a2f775e5
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-06-23 15:11:58 +0300

    Documentation for Helper functions in model.py

--- model.py
+++ model.py
@@ -536,11 +536,39 @@ def aug_m2m(model_name, text):
 ### ------------------------ Helper Functions ----------------------------- ###
 @st.cache(allow_output_mutation=True)
 def load_translator(model_name):
+  """
+  Loads the translation model from HuggingFace. It downloades and caches the model
+  for future use by the app.
+
+  Input Parameters
+  ================
+  model_name => HuggingFace link of the model (typically like this: Helsinki-NLP/opus-mt-ar-en).
+
+  Return Parameters
+  =================
+  model => Loaded model for translation.
+  tokenizer => Tokenizer for the translation model.
+  """
+
   tokenizer = MarianTokenizer.from_pretrained(model_name)
   model = MarianMTModel.from_pretrained(model_name)
   return model, tokenizer
 
 def translate_user_text_input(user_input):
+  """
+  Function translates the user text input from arabic to english using a Helsinki-NLP model
+  from HuggingFace. The model translates the sentence and then sends it to the frontend for 
+  displaying.
+
+  Input Parameters
+  ================
+  user_input => Arabic sentence for translation (given by the user).
+
+  Return Parameters
+  =================
+  translated_sentence => Returns the English translation of the arabic sentence back to the front-end.
+  """
+  
   model, tokenizer = load_translator("Helsinki-NLP/opus-mt-ar-en")
   translated = model.generate(**tokenizer(user_input, return_tensors="pt", padding=True))
   translated_sentence = ""
@@ -548,19 +576,22 @@ def translate_user_text_input(user_input):
     translated_sentence = tokenizer.decode(t, skip_special_tokens=True)
   return translated_sentence
 
-def process(text):
-  # remove any punctuations in the text
-  punc = """،.:!?؟!:.,''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~"""
-  text = text.strip()
-  if text[-1] in punc:
-      text = text[0:-1]
-  text = text.strip()
-  # keep only arabic text
-  text = " ".join(re.findall(r'[\u0600-\u06FF]+', text))
-  return text
+def is_replacable(token, pos_dict):
+  """
+  Helper function which tells the augmenting functions if we can removes all the uncessary 
+  parts of a sentence and keeps only the NOUNS, VERBS and ADJECTIVES of the sentences for 
+  augmentation. The parts of the sentence are coming from the POS tagger from Farasa API.
 
-def is_replacable(token,pos_dict):
-  # removes all the unncessary part of a sentence and keeps only the main ones for augmentation 
+  Input Parameters
+  ================
+  token => Token from POS tagging.
+  pos_dict => Parts of the Speech in the sentence.
+
+  Return Parameters
+  =================
+  True/False (bool value) => tells the user whether there are POS which can be replaced.
+  """
+
   if ner(token) != 'O':
      return False
   if token in pos_dict:
@@ -569,6 +600,24 @@ def is_replacable(token,pos_dict):
   return False
 
 def spl(text):
+  """
+  Helper function to split the sentence into three parts, first part, second part and augmented
+  word. This helps to color the augmented words in the front-end.
+
+  The augmented word is marked by a star from the augmenting functions and the star is removed
+  and then sentence is split into 3 to be processed in the frontend.
+
+  Input Parameters
+  ================
+  text => Sentence for splitting.
+
+  Return Parameters
+  =================
+  rep => Augmented Word (Will be coloured in the frontend).
+  fhalf => First half of the sentence.
+  shalf => Second half of the sentence (after the augmented word).
+  """
+
   fhalf = []
   shalf = []
   rep = ""
@@ -585,7 +634,7 @@ def spl(text):
   shalf = " ".join(shalf)
   return rep, fhalf, shalf
 
-def ner(text):
+def ner(text): #TODO: Documentation
   url = 'https://farasa.qcri.org/webapi/ner/'
   api_key = "KMxvdPGsKHXQAbRXGL"
   payload = {'text': text, 'api_key': api_key}
@@ -594,11 +643,36 @@ def ner(text):
   return result['text'][0].split("/")[1]
 
 def models_data(file_name):
+  """
+  Helper function to open a JSON file and return the data to the front-end.
+
+  Input Parameter
+  ===============
+  file_name => File name of the JSON file to be opened
+
+  Return Parameter
+  ================
+  data => Data from the file returned to the frontend
+  """
+
   f = open(file_name)
   data = json.load(f)
   return data
 
 def seperate_punct(text):
+  """
+  Helper function to seperate the punctuations and not the arabic characters. The arabic characters are
+  imported from the AR_LETTERS_CHARSET. 
+
+  Input Parameters
+  ================
+  text => Takes a sentence for seperating the punctuations.
+
+  Return Parameters
+  =================
+  ret => Returns the sentence without punctuations.
+  """
+
   text = text.strip()
   text = " ".join(text.split())
   ret = ""
@@ -616,7 +690,19 @@ def seperate_punct(text):
   return ret
 
 def clean(text):
-  # remove any punctuations in the text
+  """
+  Helper function which process the arabic text and removes any and all punctutaion 
+  and keeps only the arabic text. 
+
+  Input Parameters
+  ================
+  text => Sentence for processing.
+
+  Return Parameters
+  =================
+  text => Returns the clean text after processing.
+  """
+
   punc = """،.:!?؟!:.,''!"#$%&'()*+, -./:;<=>?@[\]^_`{|}~"""
   for l in text:
     if l in punc and l != " ":
@@ -626,6 +712,17 @@ def clean(text):
   return text
 
 def strip_punc(text):
+  """
+  Helper function to strip the sentence of punctuations and replace it with a space.
+
+  Input Parameters
+  ================
+  text => A sentence to remove punctuations from.
+
+  Return Parameter
+  ================
+  text => A sentence without any punctuations and all of them are replaced with a space.
+  """
   remove = ""
   for l in reversed(text):
     if l in AR_LETTERS_CHARSET:
