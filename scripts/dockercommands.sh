b2a22cea0f9a772f4f9ac5725be22a28f25a5548
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-06-30 14:44:44 +0300

    docker script change to give default name while building

--- scripts/dockercommands.sh
+++ scripts/dockercommands.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+image_name="dataaug-webapp"
+
+if [ $2 ]
+then
+	image_name=$2
+fi
+
 if [ $1 == 'stop' ]
 then
 	sudo docker stop $2 && sudo docker rm $2 && sudo docker system prune -f
 elif [ $1 == 'build' ]
 then
 	cd ..
-	sudo docker build -t $2:latest . && sudo docker run -p 8080:8501 -d $2
+	sudo docker build -t $image_name:latest . && sudo docker run -p 8080:8501 -d $image_name
 fi
 
 exit 0
