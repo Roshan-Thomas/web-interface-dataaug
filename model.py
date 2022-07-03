c719036693f2907fa9ae764ea3ddf36a85c190cb
Roshan-Thomas <roshan.thomas@qatar.tamu.edu>
2022-07-03 23:31:44 +0300

    Added try and except statements for common errors in app

--- model.py
+++ model.py
@@ -671,41 +671,51 @@ def similarity_checker(sentences, user_text_input):
         'sentence-transformers/bert-base-nli-mean-tokens')
     average_similarity = 0
 
-    if (len(sentences) > 0) and (not any(isinstance(sent, type(None)) for sent in sentences)):
-        tokens = {'input_ids': [], 'attention_mask': []}
-        sentences.insert(0, user_text_input)
-        for sentence in sentences:
-            # tokenize sentence and append to dictionary lists
-            new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,
-                                               padding='max_length', return_tensors='pt')
-            tokens['input_ids'].append(new_tokens['input_ids'][0])
-            tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+    try:
+        if (len(sentences) > 0) and (not any(isinstance(sent, type(None)) for sent in sentences)):
+            tokens = {'input_ids': [], 'attention_mask': []}
+            sentences.insert(0, user_text_input)
+            for sentence in sentences:
+                # tokenize sentence and append to dictionary lists
+                new_tokens = tokenizer.encode_plus(sentence, max_length=128, truncation=True,
+                                                   padding='max_length', return_tensors='pt')
+                tokens['input_ids'].append(new_tokens['input_ids'][0])
+                tokens['attention_mask'].append(
+                    new_tokens['attention_mask'][0])
 
-        # reformat list of tensors into single tensor
-        tokens['input_ids'] = torch.stack(tokens['input_ids'])
-        tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+            # reformat list of tensors into single tensor
+            tokens['input_ids'] = torch.stack(tokens['input_ids'])
+            tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
 
-        outputs = model(**tokens)
-        embeddings = outputs.last_hidden_state
-        attention_mask = tokens['attention_mask']
-        mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+            outputs = model(**tokens)
+            embeddings = outputs.last_hidden_state
+            attention_mask = tokens['attention_mask']
+            mask = attention_mask.unsqueeze(
+                -1).expand(embeddings.size()).float()
 
-        masked_embeddings = embeddings * mask
-        summed = torch.sum(masked_embeddings, 1)
-        summed_mask = torch.clamp(mask.sum(1), min=1e-9)
-        mean_pooled = summed / summed_mask
+            masked_embeddings = embeddings * mask
+            summed = torch.sum(masked_embeddings, 1)
+            summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+            mean_pooled = summed / summed_mask
 
-        # Convert from PyTorch tensor to numpy array
-        mean_pooled = mean_pooled.detach().numpy()
+            # Convert from PyTorch tensor to numpy array
+            mean_pooled = mean_pooled.detach().numpy()
 
-        # Calculate cosine similarity
-        cos_similarity = cosine_similarity([mean_pooled[0]], mean_pooled[1:])
+            # Calculate cosine similarity
+            cos_similarity = cosine_similarity(
+                [mean_pooled[0]], mean_pooled[1:])
 
-        # Calculate average of similarities
-        if len(sentences) >= 2:
-            average_similarity = mean(cos_similarity[0][1:])
+            # Calculate average of similarities
+            if len(sentences) >= 2:
+                try:
+                    average_similarity = mean(cos_similarity[0][1:])
+                except:
+                    st.warning(
+                        "No augmented sentences by the model. So average similarity was not calculated.")
 
-        return np.around(cos_similarity[0], decimals=6), average_similarity
+            return np.around(cos_similarity[0], decimals=6), average_similarity
+    except:
+        st.warning("No augmented sentences by the model.")
 
 
 def display_similarity_table(sentences_list, similarity_list, model_name):
@@ -721,17 +731,19 @@ def display_similarity_table(sentences_list, similarity_list, model_name):
     sentences_list => List of augmented sentences.
     similarity_list => List of the cosine similarities 
     """
-
-    if (len(sentences_list) > 0) and (not any(isinstance(sent, type(None)) for sent in sentences_list)):
-        data = list(zip(sentences_list, similarity_list))
-        df = pd.DataFrame(data, columns=['Sentences', 'Similarity Score'])
-        csv_file = convert_df_to_csv(df)
-        st.download_button(
-            label=f"Download {model_name} results as a CSV",
-            data=csv_file,
-            file_name=f'{model_name}-output.csv',
-            mime='text/csv',
-        )
-        st.table(df[1:].style.background_gradient(cmap='Greens'))
+    try:
+        if (len(sentences_list) > 0) and (not any(isinstance(sent, type(None)) for sent in sentences_list)):
+            data = list(zip(sentences_list, similarity_list))
+            df = pd.DataFrame(data, columns=['Sentences', 'Similarity Score'])
+            csv_file = convert_df_to_csv(df)
+            st.download_button(
+                label=f"Download {model_name} results as a CSV",
+                data=csv_file,
+                file_name=f'{model_name}-output.csv',
+                mime='text/csv',
+            )
+            st.table(df[1:].style.background_gradient(cmap='Greens'))
+    except:
+        st.warning(f"No augmented sentences by {model_name}.")
 
 ### -------------------- End of Similarity Checker ------------------------- ###
